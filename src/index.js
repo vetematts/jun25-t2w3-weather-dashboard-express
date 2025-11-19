5a0b899c72746e5c5f0e4dacc455180208fbb34f
APaud3l <aamod.paudel@coderacademy.edu.au>
2025-11-19 20:43:37 +1100

    feat: Implemented Login route, generated JWT and verified the token

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const rateLimit = require('express-rate-limit');
 const app = express();
 
 app.use(express.json());
+
+const JWT_SECRET = process.env.JWT_SECRET || 'dummykey';
 
 // Limit to 5 /weather requests per minute per IP.
 const weatherLimiter = rateLimit({
@@ -74,7 +77,45 @@ app.get('/', (request, response) => {
     });
 });
 
-app.post('/weather', dummyAuth, checkAdminRole, validateCoords, blockAntartica, async (request, response, next) => {
+app.post('/login', (request, response) => {
+    const {username, password} = request.body;
+
+    // Simulate user verification (replace it with real verification)
+    if (username == 'user' && password == 'pass') {
+        const payload = { username };
+        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '5m' });
+        return response.json({
+            token
+        });
+    }
+    response.status(401).json({
+        error: 'Invalid credentials'
+    });
+});
+
+authenticateToken = (request, response, next) => {
+    const authHeader = request.headers['authorization'];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (!token) {
+        response.status(401).json({
+            error: 'Token is missing.'
+        });
+    }
+
+    jwt.verify(token, JWT_SECRET, (error, user) => {
+        if (error) {
+            return response.status(403).json({
+                error: 'Invalid or expired token'
+            });
+        }
+        request.user = user;
+        next();
+    })
+}
+
+app.post('/weather', authenticateToken, validateCoords, blockAntartica, async (request, response, next) => {
     const { latitude, longitude } = request.body;
     try {
         const weatherResponse = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current`);
